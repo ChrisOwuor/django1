6449c05113f2aa30c85b09d2b372041f5b453b44
Chrispine Owuor <104433015+ChrisOwuor@users.noreply.github.com>
2023-10-23 14:48:35 +0300

    Update views.py

--- teachers/views.py
+++ teachers/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def Home(request):
-    return HttpResponse("welcome teahers")
+    return HttpResponse("welcome teachers")
 
 def Services(request):
     return HttpResponse("Below are our services")
